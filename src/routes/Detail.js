93a8ff939e283502e265c5c35c6bca679142a7d1
PsmD <tbvjeld7@naver.com>
2021-12-07 14:56:05 +0900

    Detail route에서 component 분리

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,14 +1,19 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import styles from "./Detail.module.css";
+import MovieDetail from "../components/MovieDetail";
+import Loading from "../components/Loading";
+import styles from "./Home.module.css";
+
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
   const [detail, setDetail] = useState([]);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setDetail(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
@@ -16,15 +21,21 @@ function Detail() {
   console.log(detail);
   return (
     <div className={styles.container}>
-      <img src={detail.medium_cover_image}></img>
-      <div className={styles.description}>
-      <h1 className={styles.description}>{detail.title_long}</h1>
-      <p>Rating : {detail.rating}</p>
-      <p>Runtime : {detail.runtime}</p>
-      <p>download : {detail.download_count}</p>
-      <ul>{detail.genres.map((gg) => (<li key={gg}>{gg}</li>))}</ul>
-      </div>
-    </div>
-  );
+    {loading ? (
+        <Loading />
+    ) : (
+        <MovieDetail
+            background_image_original={detail.background_image_original}
+            medium_cover_image={detail.medium_cover_image}
+            url={detail.url}
+            title_long={detail.title_long}
+            rating={detail.rating}
+            runtime={detail.runtime}
+            genres={detail.genres}
+            download_count={detail.download_count}             
+        />
+    )}
+</div>
+);
 }
 export default Detail;
