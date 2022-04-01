0f067cf8b0c80cedfdb76b1e67fa877876e272bd
PsmD <tbvjeld7@naver.com>
2022-04-01 22:22:25 +0900

    refactor: recoil atom 분리

---
+++ src/atom/NavMenu.js
@@ -0,0 +1,4 @@
+const Menu_obj = { "High Rating": "minimum_rating=8", Drama: "genre=drama", Action: "genre=action", Animation: "genre=animation" };
+const Menu_key_arr = Object.keys(Menu_obj);
+
+export { Menu_obj, Menu_key_arr };
