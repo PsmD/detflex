c5beee124fbd4a084d43cd034902ba4d7c5fc78d
PsmD <tbvjeld7@naver.com>
2022-04-01 22:26:12 +0900

    refactor: Home에 관한 코드 정리

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,13 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 import styles from "./Home.module.css";
-import {GrInstagram} from 'react-icons/gr';
-import {FiTwitter} from 'react-icons/fi';
-import {GiAnimalSkull} from 'react-icons/gi';
+import Loading from "../components/Loading";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
+    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)).json();
     setMovies(json.data.movies);
     setLoading(false);
     console.log(json);
@@ -23,27 +17,8 @@ function Home() {
   }, []);
   return (
     <div className={styles.container}>
-      <nav className={styles.bar}>
-        <div className={styles.logo}>
-        <GiAnimalSkull/>
-        <h2>Movie World</h2>
-            </div>
-        <div className={styles.menu}>
-        <ul>
-          <li>Top Rating</li>
-          <li>News</li>
-          <li>Community</li>
-        </ul>
-          </div>
-        <div className={styles.social}>
-        <FiTwitter/>
-        <GrInstagram/>
-          </div>
-        </nav>
       {loading ? (
-        <div className={styles.loader}>
-          <span>Loading...</span>
-        </div>
+        <Loading />
       ) : (
         <div className={styles.movies}>
           {movies.map((movie) => (
