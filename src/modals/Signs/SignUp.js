c1cabe93e349ff9ab942a0a5964092581afe92d8
PsmD <tbvjeld7@naver.com>
2022-04-03 23:53:09 +0900

    refactor: Signs 세팅

---
+++ src/modals/Signs/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./SignUp.module.css";
+import Modal from "../Modal";
+
+function SignUp() {
+  return (
+    <div>
+      <Modal>signUp</Modal>
+    </div>
+  );
+}
+
+export default SignUp;
