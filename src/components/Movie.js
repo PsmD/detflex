aa70ab972d3d38f4fabe8835cc4523b277385621
PsmD <tbvjeld7@naver.com>
2022-04-05 23:51:03 +0900

    feat: 길이가 긴 title slice 처리

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,21 +4,22 @@ import styles from "./Movie.module.css";
 
 function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div className={styles.movie}>
-      <img src={coverImg} alt={title} className={styles.movie__img} />
-      <div>
-        <h2 className={styles.movie__title}>
-          <Link to={`/movie/${id}`}>{title}</Link>
-        </h2>
-        <h3 className={styles.movie__year}>{year}</h3>
-        <p>{summary.length > 100 ? `${summary.slice(0, 100)}...` : summary}</p>
-        <ul className={styles.movie__genres}>
-          {genres.map((g) => (
-            <li key={g}>{g}</li>
-          ))}
-        </ul>
+    <Link to={`/movie/${id}`}>
+      <div className={styles.movie}>
+        <img src={coverImg} alt={title} className={styles.movie__img} />
+
+        <div>
+          <h2 className={styles.movie__title}>{title.length > 20 ? `${title.slice(0, 20)}...` : title}</h2>
+          <h3 className={styles.movie__year}>{year}</h3>
+          <p>{summary.length > 100 ? `${summary.slice(0, 100)}...` : summary}</p>
+          <ul className={styles.movie__genres}>
+            {genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
