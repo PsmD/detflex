1088902f349eb3adbdad3e6fc463bb7491bb198b
PsmD <tbvjeld7@naver.com>
2022-04-08 23:38:07 +0900

    design: Movie cards style 설정

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,20 +2,17 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styles from "./MovieCard.module.css";
 
-function MovieCard({ id, coverImg, title, year, genres }) {
+function MovieCard({ id, coverImg, title, year, rating, runtime }) {
   return (
     <Link to={`/movie/${id}`}>
       <div className={styles.movie}>
         <img src={coverImg} alt={title} className={styles.movie__img} />
 
-        <div>
-          <h2 className={styles.movie__title}>{title.length > 20 ? `${title.slice(0, 20)}...` : title}</h2>
-          <h3 className={styles.movie__year}>{year}</h3>
-          <ul className={styles.movie__genres}>
-            {genres.map((g) => (
-              <li key={g}>{g}</li>
-            ))}
-          </ul>
+        <div className={styles.movie__textBox}>
+          <div className={styles.movie__title}>{title.length > 18 ? `${title.slice(0, 18)}...` : title}</div>
+          <span className={styles.movie__year}>{year}</span>
+          <span className={styles.movie__rating}>Rating: {rating} / 10</span>
+          <span className={styles.movie__runtime}>Runtime: {runtime} min</span>
         </div>
       </div>
     </Link>
