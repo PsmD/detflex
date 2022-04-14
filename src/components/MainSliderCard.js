6fae850a8d5b53901815577a7611d1ef9f805e01
PsmD <tbvjeld7@naver.com>
2022-04-14 23:56:22 +0900

    feat: swiper 슬라이더 구현(v3)

--- src/components/MainSliderCard.js
+++ src/components/MainSliderCard.js
@@ -2,11 +2,15 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-function MainSliderCard({ id, large_cover_image }) {
+function MainSliderCard({ id, img, title, overview }) {
   return (
     <>
+      <TextBox>
+        <Title>{title}</Title>
+        <Overview>{overview.length > 160 ? `${overview.slice(0, 160)}...` : overview}</Overview>
+      </TextBox>
       <Link to={`/movie/${id}`}>
-        <MovieImg img={large_cover_image} />
+        <MovieImg src={img} />
       </Link>
     </>
   );
@@ -14,15 +18,24 @@ function MainSliderCard({ id, large_cover_image }) {
 
 MainSliderCard.prototypes = {
   id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  large_cover_image: PropTypes.string.isRequired,
+  backdrop_path: PropTypes.string.isRequired,
 };
 
 export default MainSliderCard;
 
-const MovieImg = styled.div`
-  height: 70vh;
-  width: 60vw;
-  background-image: linear-gradient(to left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url("${(props) => props.img}");
-  background-size: 100% 100%;
+const MovieImg = styled.img`
+  height: 90vh;
+  width: 100vw;
+`;
+
+const TextBox = styled.div`
+  position: absolute;
+`;
+
+const Title = styled.div`
+  color: white;
+`;
+
+const Overview = styled.div`
+  color: white;
 `;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,38 +1,31 @@
 import { useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 import Loading from "../components/Loading";
 import axios from "axios";
 import MainSliderCard from "../components/MainSliderCard";
-import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import MainSliderTextBox from "../components/MainSliderTextBox";
+// import { API_KEY, BASE_PATH, IMAGE_BASE_URL } from "../api";
 import styled from "styled-components";
-
-const rowVariants = {
-  hidden: (back) => ({
-    x: back ? -window.outerWidth - 5 : window.outerWidth + 5,
-  }),
-  visible: {
-    x: 0,
-  },
-  exit: (back) => ({
-    x: back ? window.outerWidth + 5 : -window.outerWidth - 5,
-  }),
-};
+import { Navigation, Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  const [index, setIndex] = useState(0);
-  const [leaving, setLeaving] = useState(false);
-  const [back, setBack] = useState(false);
+
+  const API_KEY = "c1322c5f03b8623fbdaf33cb57f9df22";
+
+  const BASE_PATH = "https://api.themoviedb.org/3";
+
+  const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 
   const getMovies = async () => {
     await axios
-      .get("https://yts.mx/api/v2/list_movies.json?&limit=5&sort_by=rating")
+      .get(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`)
       .then((res) => {
-        setMovies(res.data.data.movies);
+        console.log(res.data.results);
+        setMovies(res.data.results);
         setLoading(false);
       })
       .catch((err) => {
@@ -46,68 +39,34 @@ function Home() {
     return;
   }, []);
 
-  const toggleLeaving = () => setLeaving((prev) => !prev);
-
-  const increaseIndex = () => {
-    if (movies) {
-      if (leaving) return;
-      toggleLeaving();
-      const total = 4;
-      setBack(false);
-      setIndex((prev) => (prev === total ? 0 : prev + 1));
-    }
-  };
-  const decreaseIndex = () => {
-    if (movies) {
-      if (leaving) return;
-      toggleLeaving();
-      const total = 0;
-      setBack(true);
-      setIndex((prev) => (prev === total ? 4 : prev - 1));
-    }
-  };
   return (
     <Container>
       {loading ? (
         <Loading />
       ) : (
-        <Slider>
-          <AnimatePresence initial={false} custom={back} onExitComplete={toggleLeaving}>
-            <Row
-              variants={rowVariants}
-              initial="hidden"
-              animate="visible"
-              exit="exit"
-              transition={{ type: "tween", duration: 0.5 }}
-              custom={back}
-              key={index}
-            >
-              {movies?.slice(index, index + 1).map((movie) => (
+        <StyledSwiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={0}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          allowTouchMove={false}
+          loop={true}
+          autoplay={{ delay: 5000, disableOnInteraction: false, pauseOnMouseEnter: true }}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {movies &&
+            movies.slice(0, 5).map((movie) => (
+              <SwiperSlide key={movie.id}>
                 <MainSliderCard
-                  key={movie.id}
-                  id={movie.id}
-                  large_cover_image={movie.large_cover_image}
-                ></MainSliderCard>
-              ))}
-            </Row>
-            <TextBox>
-              {movies?.slice(index, index + 1).map((movie) => (
-                <MainSliderTextBox
-                  key={movie.id}
                   id={movie.id}
                   title={movie.title}
-                  summary={movie.summary}
-                ></MainSliderTextBox>
-              ))}
-            </TextBox>
-          </AnimatePresence>
-          <NextButton onClick={increaseIndex}>
-            <FontAwesomeIcon icon={faAngleRight} size="lg" />
-          </NextButton>
-          <PrevButton onClick={decreaseIndex}>
-            <FontAwesomeIcon icon={faAngleLeft} size="lg" />
-          </PrevButton>
-        </Slider>
+                  overview={movie.overview}
+                  img={`${IMAGE_BASE_URL}original${movie.backdrop_path}`}
+                ></MainSliderCard>
+              </SwiperSlide>
+            ))}
+        </StyledSwiper>
       )}
     </Container>
   );
@@ -118,57 +77,50 @@ export default Home;
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  display: flex;
   padding: 0;
   margin: 0;
-  justify-content: center;
-  align-items: center;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Slider = styled.div`
-  width: 100vw;
-  height: 70vh;
-  margin-top: 20vh;
+const StyledSwiper = styled(Swiper)`
+  margin-top: 10vh;
   position: relative;
   display: flex;
-  justify-content: end;
-  align-items: center;
-`;
+  width: 100vw;
+  height: 90vh;
 
-const Row = styled(motion.div)`
-  position: absolute;
-  max-width: 100vw;
-`;
+  .swiper-pagination-bullet {
+    width: 20px;
+    height: 3px;
+    border-radius: 10px;
+    background-color: #fff;
+  }
 
-const BaseButton = styled.div`
-  position: absolute;
-  width: 40px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  user-select: none;
-  cursor: pointer;
-  font-weight: bold;
-  font-size: 30px;
-  z-index: 2;
-`;
+  .swiper-pagination-bullet-active {
+    transition: 0.2s;
+    width: 60px;
+    background-color: #fff;
+  }
 
-const NextButton = styled(BaseButton)`
-  right: 10px;
-`;
+  .swiper-button-next.swiper-button-disabled,
+  .swiper-button-prev.swiper-button-disabled {
+    opacity: 0;
+  }
 
-const PrevButton = styled(BaseButton)`
-  left: 10px;
-`;
-
-const TextBox = styled.div`
-  position: absolute;
-  width: 60vw;
-  height: 70vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  left: 0;
-  background: linear-gradient(to right, red 40vw, rgba(0, 0, 0, 0));
+  .swiper-button-next,
+  .swiper-button-prev {
+    width: 80px;
+    height: 80px;
+    background-color: rgba(240, 240, 240, 0.7);
+    backdrop-filter: blur(3px);
+    background-size: 40px;
+    background-position: center;
+    background-repeat: no-repeat;
+    z-index: 2;
+    border-radius: 50%;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    position: absolute;
+  }
 `;
