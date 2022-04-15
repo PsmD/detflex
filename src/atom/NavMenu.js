047705660937ff2a5f1ce1ca286577114b65457d
PsmD <tbvjeld7@naver.com>
2022-04-15 23:51:15 +0900

    feat: navbar menu 변경

--- src/atom/NavMenu.js
+++ src/atom/NavMenu.js
@@ -1,4 +1,9 @@
-const MovieMenu_obj = { "High Rating": "minimum_rating=8", Drama: "genre=drama", Action: "genre=action", Animation: "genre=animation" };
+const MovieMenu_obj = {
+  "Top Rating": "top_rated",
+  "Now Playing": "now_playing",
+  Popular: "popular",
+  "Up Coming": "upcoming",
+};
 const MovieMenu_key_arr = Object.keys(MovieMenu_obj);
 
 export { MovieMenu_obj, MovieMenu_key_arr };
