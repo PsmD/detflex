5c3124be60a5f1210064e48328cf5aa83be9e961
PsmD <tbvjeld7@naver.com>
2022-04-15 23:50:24 +0900

    style: 파일 위치 변경

--- api.js
+++
@@ -1,7 +0,0 @@
-const API_KEY = "c1322c5f03b8623fbdaf33cb57f9df22";
-
-const BASE_PATH = "https://api.themoviedb.org/3";
-
-const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
-
-export default api;
---
+++ src/api.js
@@ -0,0 +1,5 @@
+export const API_KEY = "c1322c5f03b8623fbdaf33cb57f9df22";
+
+export const BASE_PATH = "https://api.themoviedb.org/3";
+
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
