076dc1a244d918776fc2bd227b638778185dd240
PsmD <tbvjeld7@naver.com>
2022-04-22 22:14:12 +0900

    feat: 404페이지 세팅

---
+++ src/routes/NotFound.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function NotFound() {
+  return <InvalidPath>404 error Invalid path.</InvalidPath>;
+}
+export default NotFound;
+
+const InvalidPath = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
