c7e1ca5820ef549a4af563fee8f480e0eb5849bc
PsmD <tbvjeld7@naver.com>
2022-04-29 23:33:16 +0900

    feat: import getFirestore

--- src/AboutFirebase/fbase.js
+++ src/AboutFirebase/fbase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID,
+  apiKey: "AIzaSyD2l-pFG7S9NEC16U3GdCS62eQ2OxtQYU8",
+  authDomain: "detflex-23384.firebaseapp.com",
+  projectId: "detflex-23384",
+  storageBucket: "detflex-23384.appspot.com",
+  messagingSenderId: "655919905516",
+  appId: "1:655919905516:web:f67d53c8cde5f22fb543c9",
+  measurementId: "G-ZTZNNXF7JY",
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 export const authService = getAuth();
+export const dbService = getFirestore();
