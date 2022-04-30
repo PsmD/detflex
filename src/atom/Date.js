55168c1878866d6c31eae48f78ae790f56b07318
PsmD <tbvjeld7@naver.com>
2022-04-30 22:27:00 +0900

    feat: 현재 시간, 분 추가

--- src/atom/Date.js
+++ src/atom/Date.js
@@ -5,7 +5,10 @@ const nextyear = today.getFullYear() + 1;
 const month = ("0" + (today.getMonth() + 1)).slice(-2);
 const prevmonth = ("0" + today.getMonth()).slice(-2);
 const date = ("0" + today.getDate()).slice(-2);
+const hours = ("0" + today.getHours()).slice(-2);
+const minutes = ("0" + today.getMinutes()).slice(-2);
 
 export const currentday = year + "-" + month + "-" + date;
+export const currentTime = year + "." + month + "." + date + "." + " " + hours + ":" + minutes;
 export const prevmonthday = year + "-" + prevmonth + "-" + date;
 export const nextyearday = nextyear + "-" + month + "-" + date;
