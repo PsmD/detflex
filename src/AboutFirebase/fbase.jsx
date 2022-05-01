026832c11a83b6115a80c43613716e546070e43c
PsmD <tbvjeld7@naver.com>
2022-05-01 14:06:28 +0900

    style: js -> jsx 변경

---
+++ src/AboutFirebase/fbase.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+initializeApp(firebaseConfig);
+export const authService = getAuth();
+export const dbService = getFirestore();
