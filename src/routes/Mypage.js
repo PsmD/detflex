1688cda98ea1e382e513c0d8c3560d9c2df0cff1
PsmD <tbvjeld7@naver.com>
2022-05-18 21:26:25 +0900

    feat: 마이페이지 구현 진행

--- src/routes/Mypage.js
+++ src/routes/Mypage.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import { UserContext } from "../AboutFirebase/UseAuth";
 import { useParams } from "react-router-dom";
-import { useState, useEffect, useCallback, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { dbService } from "../AboutFirebase/fbase";
-import { addDoc, onSnapshot, collection, query, where, orderBy, getDocs } from "firebase/firestore";
+import { collection, query, where, orderBy, getDocs } from "firebase/firestore";
 import Loading from "../components/Loading";
 import axios from "axios";
 import { API_KEY, BASE_PATH, IMAGE_BASE_URL } from "../api";
@@ -12,33 +12,55 @@ import MovieCard from "../components/Cards/MovieCard";
 function MyPage() {
   const [likedMovies, setLikedMovies] = useState([]);
   const [loading, setLoading] = useState(true);
-  const currentUser = useContext(UserContext);
+  const user = useContext(UserContext);
   const { movieId } = useParams();
+  const [userLikeMovies, setUserLikeMovies] = useState([]);
+  const userLikeObject = userLikeMovies.find((userLikeObject) => userLikeObject.creatorId === user.user.uid);
 
   const fetchData = async () => {
     const LikedQuerydRef = collection(dbService, "likes");
-    const LikedQuery = query(
-      LikedQuerydRef,
-      where("creatorId", "==", currentUser.user.uid),
-      orderBy("createdAt", "desc")
-    );
+    const LikedQuery = query(LikedQuerydRef, where("creatorId", "==", user.user.uid), orderBy("createdAt", "desc"));
     const LikedSnapShot = await getDocs(LikedQuery);
     const data = LikedSnapShot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-    console.log(data);
-    return data;
+    setUserLikeMovies(data);
+    console.log(userLikeMovies);
+    setLoading(false);
   };
+
+  const getLikedMovie = async () => {
+    await axios.get(`${BASE_PATH}/movie/${userLikeMovies.detailMovieId}?api_key=${API_KEY}`).then((res) => {
+      setLikedMovies(res.data);
+      console.log(res.data);
+    });
+  };
+
   useEffect(() => {
     fetchData();
+    getLikedMovie();
   }, []);
 
   return (
-    <Container>
-      <>
-        <div>{currentUser.user.email}</div>
-        <div>{currentUser.user.displayName}</div>
-        <div>{fetchData.creatorId}</div>
-      </>
-    </Container>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Container>
+          <div>{user.user.email}</div>
+          <div>{user.user.displayName}</div>
+          <div>{userLikeObject.creatorId}</div>
+          {likedMovies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              movieId={movie.id}
+              title={movie.title}
+              poster_path={`${IMAGE_BASE_URL}original${movie.poster_path}`}
+              year={movie.release_date}
+              vote_average={movie.vote_average}
+            />
+          ))}
+        </Container>
+      )}
+    </>
   );
 }
 export default MyPage;
