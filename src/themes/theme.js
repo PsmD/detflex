ef8965ed6e4c75f8528a06e6e027ebecf43edda4
PsmD <tbvjeld7@naver.com>
2022-05-21 22:43:43 +0900

    feat: mediaquery 반응형 웹 세팅

---
+++ src/themes/theme.js
@@ -0,0 +1,19 @@
+const deviceSizes = {
+  mobile: "375px",
+  tablet: "768px",
+  laptop: "1536px",
+  desktop: "1920px",
+};
+
+const device = {
+  mobile: `screen and (max-width: ${deviceSizes.mobile})`,
+  tablet: `screen and (max-width: ${deviceSizes.tablet})`,
+  laptop: `screen and (max-width: ${deviceSizes.laptop})`,
+  desktop: `screen and (max-width: ${deviceSizes.laptop})`,
+};
+
+const theme = {
+  device,
+};
+
+export default theme;
