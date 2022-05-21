8aee4f2e170d8fa280fc61d4f53727998c0aa194
PsmD <tbvjeld7@naver.com>
2022-05-21 22:44:39 +0900

    feat: ThemeProvider 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import theme from "./themes/theme";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import Navbar from "./components/Navbar";
@@ -12,19 +14,21 @@ import NoSearch from "./routes/NoSearch";
 function App() {
   return (
     <RecoilRoot>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={`/page/:menu`} element={<MovieMenu />} />
-          <Route path={`/movie/:movieId`} element={<Detail />} />
-          <Route path={`/search/:searchText`} element={<Search />} />
-          <Route path={"/my_page"} element={<MyPage />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="/nosearch/:searchText" element={<NoSearch />} />
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={`/page/:menu`} element={<MovieMenu />} />
+            <Route path={`/movie/:movieId`} element={<Detail />} />
+            <Route path={`/search/:searchText`} element={<Search />} />
+            <Route path={"/my_page"} element={<MyPage />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="/nosearch/:searchText" element={<NoSearch />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
