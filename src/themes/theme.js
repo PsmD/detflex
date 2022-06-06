05988b8f9267131e76befeef44ccedfce7c84111
PsmD <tbvjeld7@naver.com>
2022-06-06 22:58:20 +0900

    style: deviceSizes 추가

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,6 +1,7 @@
 const deviceSizes = {
   smaller: "500px",
   small: "900px",
+  navSmall: "1200px",
   laptop: "1530px",
   desktop: "1900px",
 };
@@ -8,6 +9,7 @@ const deviceSizes = {
 const device = {
   smaller: `screen and (max-width: ${deviceSizes.smaller})`,
   small: `screen and (max-width: ${deviceSizes.small})`,
+  navSmall: `screen and (max-width: ${deviceSizes.navSmall})`,
   laptop: `screen and (min-width: ${deviceSizes.laptop})`,
   desktop: `screen and (min-width: ${deviceSizes.desktop})`,
 };
