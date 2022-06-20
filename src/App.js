82e512702d75b475e179b413e40dc94a7b99d14a
PsmD <tbvjeld7@naver.com>
2022-06-20 20:19:58 +0900

    style: 불필요한 코드 제거

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { RecoilRoot } from "recoil";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import theme from "./themes/theme";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import MovieMenu from "./routes/MovieMenu";
 import Search from "./routes/Search";
 import NotFound from "./routes/NotFound";
@@ -12,24 +11,22 @@ import MyPage from "./routes/Mypage";
 import NoSearch from "./routes/NoSearch";
 
 function App() {
-  return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path={"/"} element={<Home />} />
-            <Route path={`/page/:menu`} element={<MovieMenu />} />
-            <Route path={`/movie/:movieId`} element={<Detail />} />
-            <Route path={`/search/:searchText`} element={<Search />} />
-            <Route path={"/my_page"} element={<MyPage />} />
-            <Route path="/404" element={<NotFound />} />
-            <Route path="/nosearch/:searchText" element={<NoSearch />} />
-            <Route path="*" element={<Navigate to="/404" replace />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </RecoilRoot>
-  );
+	return (
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<Navbar />
+				<Routes>
+					<Route path={"/"} element={<Home />} />
+					<Route path={`/page/:menu`} element={<MovieMenu />} />
+					<Route path={`/movie/:movieId`} element={<Detail />} />
+					<Route path={`/search/:searchText`} element={<Search />} />
+					<Route path={"/my_page"} element={<MyPage />} />
+					<Route path="/404" element={<NotFound />} />
+					<Route path="/nosearch/:searchText" element={<NoSearch />} />
+					<Route path="*" element={<Navigate to="/404" replace />} />
+				</Routes>
+			</ThemeProvider>
+		</BrowserRouter>
+	);
 }
 export default App;
